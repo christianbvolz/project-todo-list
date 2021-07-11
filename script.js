ccd6da876f974a30206c78e1f7570079ff0ba79d
Christianbv <christianbvolz@gmail.com>
2021-07-11 15:51:46 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-document.getElementById('criar-tarefa').addEventListener('click', function addTask() {
+function cleanSelectItem() {
+  document.querySelectorAll('.list-item').forEach((element) => {
+    const ele = element;
+    ele.className = 'list-item';
+  });
+}
+
+function setListItem() {
+  document.querySelectorAll('.list-item').forEach((element) => {
+    element.addEventListener('click', () => {
+      const ele = element;
+      cleanSelectItem();
+      ele.className = 'list-item selected-item';
+    });
+  });
+}
+function addTask() {
   const input = document.getElementById('texto-tarefa');
-  const list = document.getElementById('lista-tarefas');
-  let itemList = document.createElement('li');
-  itemList.innerText = input.value;
-  list.appendChild(itemList);
-  input.value = '';
+  if (input.value.length === 0) {
+    alert('insira uma terefa.');
+  } else {
+    const list = document.getElementById('lista-tarefas');
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerText = input.value;
+    list.appendChild(listItem);
+    input.value = '';
+    setListItem();
+  }
+}
+
+document.getElementById('criar-tarefa').addEventListener('click', addTask);
+document.getElementById('texto-tarefa').addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) {
+    addTask();
+  }
 });
+
+// document.querySelectorAll('.color').forEach((color) => {
+//   const selectedColor = color;
+//   selectedColor.addEventListener('click', () => {
+//     const allColors = document.querySelectorAll('.color');
+//     allColors.forEach((colors) => {
+//       const unselectedColor = colors;
+//       unselectedColor.className = 'color';
+//     });
+//     selectedColor.className = 'color selected';
+//   });
+// });
