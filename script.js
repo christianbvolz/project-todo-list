b7de0d4e15e306978704054742245081ec724c85
Christianbv <christianbvolz@gmail.com>
2021-07-11 23:56:54 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function cleanSelectItem() {
-  document.querySelectorAll('.list-item').forEach((element) => {
-    const ele = element;
-    ele.className = 'list-item';
-  });
+  const selected = document.querySelector('.selected-item');
+  if (selected !== null) {
+    let newClass = selected.className.split(' ').filter((array) => array !== 'selected-item');
+    newClass = newClass.join(' ');
+    selected.className = newClass;
+  }
 }
 
-function setListItem() {
-  document.querySelectorAll('.list-item').forEach((element) => {
-    element.addEventListener('click', () => {
-      const ele = element;
-      cleanSelectItem();
-      ele.className = 'list-item selected-item';
-    });
-  });
+function selectedItem(event) {
+  const listItem = event.target;
+  cleanSelectItem();
+  listItem.className += ' selected-item';
 }
+
+// function cleancompletedItem() {
+//   const selected = document.querySelector('.completed-item');
+//   if (selected !== null) {
+//     let newClass = selected.className.split(' ').filter((array) => array !== 'completed-item');
+//     newClass = newClass.join(' ');
+//     selected.className = newClass;
+//   }
+// }
+
+function completedItem(event) {
+  const listItem = event.target;
+  if (listItem.className.includes('completed') === true) {
+    let newClass = listItem.className.split(' ').filter((array) => array !== 'completed');
+    newClass = newClass.join(' ');
+    listItem.className = newClass;
+  } else {
+    listItem.className += ' completed';
+  }
+}
+
 function addTask() {
   const input = document.getElementById('texto-tarefa');
   if (input.value.length === 0) {
@@ -25,7 +44,8 @@ function addTask() {
     listItem.innerText = input.value;
     list.appendChild(listItem);
     input.value = '';
-    setListItem();
+    listItem.addEventListener('click', selectedItem);
+    listItem.addEventListener('dblclick', completedItem);
   }
 }
 
@@ -35,15 +55,3 @@ document.getElementById('texto-tarefa').addEventListener('keypress', (event) => 
     addTask();
   }
 });
-
-// document.querySelectorAll('.color').forEach((color) => {
-//   const selectedColor = color;
-//   selectedColor.addEventListener('click', () => {
-//     const allColors = document.querySelectorAll('.color');
-//     allColors.forEach((colors) => {
-//       const unselectedColor = colors;
-//       unselectedColor.className = 'color';
-//     });
-//     selectedColor.className = 'color selected';
-//   });
-// });
